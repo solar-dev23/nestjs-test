2af2274b9db269669f9a9cda1c0ebc079b69df29
Jayden Jin <solar.dev023@gmail.com>
2021-04-11 12:59:44 -0500

    add unit test and e2e test

--- src/price/price.controller.spec.ts
+++ src/price/price.controller.spec.ts
@@ -1,18 +1,47 @@
+import * as request from 'supertest';
+import * as Chance from 'chance';
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { PriceController } from './price.controller';
+import { PriceModule } from './price.module';
 
-describe('PriceController', () => {
-  let controller: PriceController;
+describe('PriceController (e2e)', () => {
+  let app: INestApplication;
+  const chance = new Chance();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PriceController],
+      imports: [PriceModule]
     }).compile();
 
-    controller = module.get<PriceController>(PriceController);
+    app = module.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return price', () => {
+    return request(app.getHttpServer())
+      .get('/price?asset=BTC')
+      .expect(200);
+  });
+
+  it('should throw error if asset param is empty', () => {
+    return request(app.getHttpServer())
+      .get('/price?asset=')
+      .expect({
+        status: 400,
+        error: 'asset should be either BTC or ETH, not empty'
+      });
+  });
+
+  it('should throw error if asset param is invalid', () => {
+    return request(app.getHttpServer())
+      .get(`/price?asset=${chance.word()}`)
+      .expect({
+        status: 400,
+        error: 'This asset is not supported'
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
--- src/price/price.service.spec.ts
+++ src/price/price.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PriceService } from './price.service';
 
@@ -6,6 +7,11 @@ describe('PriceService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule.register({
+          baseURL: 'https://api.coingecko.com/api/v3/coins'
+        }),
+      ],
       providers: [PriceService],
     }).compile();
 
@@ -14,5 +20,14 @@ describe('PriceService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getPrice', () => {
+    it('should get price', async () => {
+      await service.getPrice('BTC').subscribe(response => {
+        expect(response.assetId).toEqual('BTC');
+        expect(typeof response.value).toBe('number');
+      });
+    });  
   });
 });

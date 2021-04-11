ff73107f82417b2665a1308abe389e530aab683a
Jayden Jin <solar.dev023@gmail.com>
2021-04-11 11:16:11 -0500

    Add error handler

--- src/price/price.controller.ts
+++ src/price/price.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, HttpException, HttpStatus } from '@nestjs/common';
 import { PriceService } from './price.service';
 import { GetPriceDto } from './dto/get-price.dto'
 
@@ -8,6 +8,20 @@ export class PriceController {
 
   @Get()
   getPrice(@Query() getPriceDto: GetPriceDto): object {
+    if (!getPriceDto.asset || getPriceDto.asset === '') {
+      throw new HttpException({
+        status: HttpStatus.BAD_REQUEST,
+        error: 'asset should be either BTC or ETH, not empty',
+      }, HttpStatus.BAD_REQUEST);
+    }
+
+    if (getPriceDto.asset !== 'BTC' && getPriceDto.asset !== 'ETH') {
+      throw new HttpException({
+        status: HttpStatus.BAD_REQUEST,
+        error: 'This asset is not supported',
+      }, HttpStatus.BAD_REQUEST);
+    }
+    
     return this.priceService.getPrice(getPriceDto.asset);
   }
 }
